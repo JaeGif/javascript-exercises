8abd1c812dd285902d3cd5cfd66de13c33de2cb0
Jae Gifford <jae.giff@outlook.com>
2022-06-13 18:44:36 +0800

    exercise 04 passed using advanced method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
-
+var removeFromArray = function(...args) {
+   const array = args[0]
+   return array.filter(val => !args.includes(val))
+}
+console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 4, 5, 6, 7, 8, 9))
 // Do not edit below this line
 module.exports = removeFromArray;

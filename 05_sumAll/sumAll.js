ac7756cfe8137215b586eda110e00a29ea269967
Jae Gifford <jae.giff@outlook.com>
2022-06-13 19:32:32 +0800

    exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+    let sum = 0;
+    if (Number.isInteger(lower) && Number.isInteger(upper)) {
+        if (lower < upper) {
+        let array = Array.from({length: upper}, (x, i) => i + lower);
+        for (element in array) {
+            sum += array[element]
+        }
+        } else if (lower > upper) {
+            let array = Array.from({length: lower}, (x, i) => i + upper);
+            for (element in array) {
+                sum += array[element]
+    }
+    }
 
+    } else {
+        return 'ERROR'
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

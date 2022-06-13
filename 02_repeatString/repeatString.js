3496559facd1d0e9c7c676d1650c86a345e9062d
Jae Gifford <jae.giff@outlook.com>
2022-06-13 17:51:12 +0800

    exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    concatString = ''
+    if (repeat  < 0) {
+        return 'ERROR'
+    }
+    for (i = 0; i < repeat; i++) {
+        concatString += string
+    }
+    return concatString
 };
 
 // Do not edit below this line

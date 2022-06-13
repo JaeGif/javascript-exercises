ff0f2b2a633baca723966e816c2d308ab30cee39
Jae Gifford <jae.giff@outlook.com>
2022-06-13 18:05:19 +0800

    exercise 03 passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("").reverse();
+    let stringNew = ''
+    for (i = 0; i < stringArray.length; i++) {
+        stringNew += stringArray[i]
+    }
+    return stringNew
 };
 
 // Do not edit below this line

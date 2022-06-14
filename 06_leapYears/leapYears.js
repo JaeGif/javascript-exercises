df927eb0e5d7e2eee62db93d56464b9bb8888a8e
Jae Gifford <jae.giff@outlook.com>
2022-06-14 09:53:07 +0800

    completed 06 with easy solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap years are divisible by 4, except century years which are divisible by 400 only.
+    // if % 4 = 0, then leap year.
+    // if year has 00 at the end, (if % 100 = 0) then if % 400 = 0 is leap year
+    if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 100 != 0 && year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
+    
 };
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;

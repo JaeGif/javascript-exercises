8605515ba837818f7662c913eabc8178d4d786c1
Jae Gifford <jae.giff@outlook.com>
2022-06-21 13:44:27 +0800

    exercise 09 done. learned regex for a more efficient evaluation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    var re = /[^A-Za-z]/g;      // regex to filter for only letters
+    str = str.toLowerCase().replace(re, '');    // convert str to all the same for == , remove filtered by regex
+    var len = str.length;       
+    for (var i = 0; i < len/2; i++) {       // every string in a palindrome is flipped across a midpoint compare letters until
+                                            // the midpoint is reached
+      
+        if (str[i] !== str[len - 1 - i]) {  // check the letters outside in if any of them don't match, evaluate false
+          return false;
+        }
+    }
+    return true;
+}
 // Do not edit below this line
 module.exports = palindromes;
